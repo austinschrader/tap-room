e11e0b78a70e7a894a05608ca5d4398c6dc866c9
Austin Schrader <austin.jschrader@gmail.com>
2021-02-24 12:20:22 -0800

    Remove unnecessary if else statement

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,35 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
-  let quantity = props.quantity;
-
-  if (quantity <= 0) {
-    return (
-      <>
-        <div onClick={() => props.whenKegClicked(props.id)}>
-          <h4>Name: {props.name} </h4>
-          <h4>Brand: {props.brand} </h4>
-          <h4>Price: {props.price} </h4>
-          <h4>ABV: {props.alcoholContent} </h4>
-          <h4>Quantity: {props.quantity} </h4>
-          {/* <h4> {props.id} </h4> */}
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div onClick={() => props.whenKegClicked(props.id)}>
-          <h4>Name: {props.name} </h4>
-          <h4>Brand: {props.brand} </h4>
-          <h4>Price: {props.price} </h4>
-          <h4>ABV: {props.alcoholContent} </h4>
-          <h4>Quantity: {props.quantity} </h4>
-          {/* <h4> {props.id} </h4> */}
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div onClick={() => props.whenKegClicked(props.id)}>
+        <h4>Name: {props.name} </h4>
+        <h4>Brand: {props.brand} </h4>
+        <h4>Price: {props.price} </h4>
+        <h4>ABV: {props.alcoholContent} </h4>
+        <h4>Quantity: {props.quantity} </h4>
+      </div>
+    </>
+  );
 }
 
 Keg.propTypes = {
